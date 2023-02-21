1b0e55681bc1d5ed78edc22cf7755c622af179ca
sonu7524 <sonudaryani135@gmail.com>
2023-02-21 23:03:30 +0530

    first commit

---
+++ src/main/java/com/driver/BookMyShow/Controllers/UserController.java
@@ -0,0 +1,7 @@
+package com.driver.BookMyShow.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ src/main/java/com/driver/BookMyShow/Models/UserEntity.java
@@ -0,0 +1,22 @@
+package com.driver.BookMyShow.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "User")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int age;
+}
